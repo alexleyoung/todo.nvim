db95ae9b465c520a8894a1eb9dd57ecfded85cab
Alex Young <alexyoung0119@gmail.com>
2025-03-01 18:14:48 -0600

    reset structure

---
+++ lua/todo/init.lua
@@ -0,0 +1,24 @@
+-- lua/todo/init.lua
+-- Main entry point for the TODO plugin
+
+local M = {}
+local storage = require("todo.storage")
+
+-- Default config
+M.config = {
+  todo_file = vim.fn.stdpath("data") .. "/todo.json", -- Path to store TODOs
+}
+
+-- Setup function
+function M.setup(user_config)
+  -- Merge user config with defaults
+  M.config = vim.tbl_extend("force", M.config, user_config or {})
+
+  -- Load existing TODOs from storage
+  storage.load_data(M.config.todo_file)
+
+  -- Set up keymaps (temporary example)
+  vim.api.nvim_set_keymap("n", "<leader>td", ":lua require('todo.ui').open()<CR>", { noremap = true, silent = true })
+end
+
+return M
