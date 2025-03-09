871d50c1afcc57ab9ed9e651192549b06c722303
Alex Young <alexyoung0119@gmail.com>
2025-03-08 21:03:49 -0600

    start new list ui

--- lua/todo/config.lua
+++ lua/todo/config.lua
@@ -4,19 +4,15 @@ local vh = 0.6 -- percentage screen height
 local vw = 0.4 -- percentage screen width
 
 -- Function to calculate window size dynamically
-M.get_window_opts = function()
-  local height = math.floor(vim.o.lines * vh)
-  local width = math.floor(vim.o.columns * vw)
-  return {
-    height = height,
-    width = width,
-    row = math.floor(vim.o.lines * ((1 - vh) / 2)),
-    col = math.floor(vim.o.columns * ((1 - vw) / 2)),
-    relative = "editor",
-    border = "rounded",
-    style = "minimal",
-  }
-end
+M.window = {
+  height = math.floor(vim.o.lines * vh),
+  width = math.floor(vim.o.columns * vw),
+  row = math.floor(vim.o.lines * ((1 - vh) / 2)),
+  col = math.floor(vim.o.columns * ((1 - vw) / 2)),
+  relative = "editor",
+  border = "rounded",
+  style = "minimal",
+}
 
 M.save_location = vim.fn.stdpath("data") .. "/todo.json" -- Path to store TODOs
 
--- lua/todo/ui.lua
+++ lua/todo/ui.lua
@@ -3,13 +3,54 @@ local M = {}
 local config = require("todo.config")
 
 M.open = function()
-  local opts = config.get_window_opts()
+  local opts = config.window
 
   local buf = vim.api.nvim_create_buf(false, true)
   local win = vim.api.nvim_open_win(buf, true, opts)
 
-  -- buf keymaps
+  -- todo buf keymaps
   vim.keymap.set("n", "q", function()
+    vim.api.nvim_win_close(win, true)
+  end, { buffer = buf, noremap = true, silent = true })
+  vim.keymap.set("n", "<C-c>", function()
+    vim.api.nvim_win_close(win, true)
+  end, { buffer = buf, noremap = true, silent = true })
+
+  vim.keymap.set("n", "a", function()
+    M.create_list()
+  end, { buffer = buf, noremap = true, silent = true })
+end
+
+-- Creates new Todo List
+M.create_list = function()
+  local opts = { relative = "cursor", width = 30, height = 1, row = 1, col = 0, border = "single" }
+
+  local buf = vim.api.nvim_create_buf(false, true)
+  local win = vim.api.nvim_open_win(buf, true, opts)
+  vim.api.nvim_buf_set_lines(buf, 0, 1, false, { "Enter list name:" })
+  vim.api.nvim_win_set_cursor(win, { 1, 0 })
+  vim.cmd("startinsert")
+
+  vim.api.nvim_buf_attach(buf, false, {
+    on_lines = function()
+      local filename = vim.api.nvim_buf_get_lines(buf, 0, -1, false)[1]
+      vim.fn.writefile({}, filename)
+      print("File created: " .. filename)
+      vim.api.nvim_win_close(0, true)
+    end,
+  })
+  -- vim.api.nvim_buf_attach(buf, false, {
+  --   on_lines = function() end,
+  -- })
+
+  -- buf keymaps to close window
+  vim.keymap.set("n", "q", function()
+    vim.api.nvim_win_close(win, true)
+  end, { buffer = buf, noremap = true, silent = true })
+  vim.keymap.set("n", "<C-c>", function()
+    vim.api.nvim_win_close(win, true)
+  end, { buffer = buf, noremap = true, silent = true })
+  vim.keymap.set("n", "<C-C>", function()
     vim.api.nvim_win_close(win, true)
   end, { buffer = buf, noremap = true, silent = true })
 end
